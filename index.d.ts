1abe11987f919260abdcad0b0349c3ef2dc79e71
Jesús Rodríguez <Foxandxss@gmail.com>
2016-08-09 19:52:05 +0200

    chore: update to RC5 and NgModule (#17)

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,6 @@
-export * from './in-memory-backend.service';
 export * from './http-status-codes';
+export * from './in-memory-backend.service';
+import { ModuleWithProviders } from '@angular/core';
+export declare class InMemoryWebApiModule {
+    static forRoot(seedData: any): ModuleWithProviders;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-export * from './in-memory-backend.service';
 export * from './http-status-codes';
+export * from './in-memory-backend.service';
+
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { HttpModule, XHRBackend } from '@angular/http';
+
+import {
+  InMemoryBackendService,
+  SEED_DATA
+} from './in-memory-backend.service';
+
+@NgModule({ exports: [ HttpModule ] })
+export class InMemoryWebApiModule {
+  static forRoot(seedData: any): ModuleWithProviders {
+    return {
+      ngModule: InMemoryWebApiModule,
+      providers: [
+        { provide: XHRBackend, useClass: InMemoryBackendService },
+        { provide: SEED_DATA, useClass: seedData }
+      ]
+    };
+  }
+}
